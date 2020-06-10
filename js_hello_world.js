6044d930da57c195e143e77d8842fabdf235eceb
Avneesh Hota <iamavneesh.hota@gmail.com>
2020-06-10 21:27:03 +0530

    clevertap push enabled

--- js_hello_world.js
+++ js_hello_world.js
@@ -1,3 +1,11 @@
+if("undefined"===typeof globalRedirectPath)var globalNotificationData,globalRedirectPath;self.addEventListener("install",function(b){self.skipWaiting();console.log("CT Service worker installed")});self.addEventListener("activate",function(b){console.log("CT Service worker activated")});
+self.addEventListener("push",function(b){console.log("Push event: ",b);var a=JSON.parse(b.data.text()),c=a.title,d=a.notificationOptions,f=d.data,e;"undefined"!==typeof f&&(e=f.wzrk_id);"undefined"===typeof e&&(e=c);localforage.setItem(e,b.data.text()).then(function(a){})["catch"](function(a){console.log("Error in persisting")});globalRedirectPath=a.redirectPath;globalNotificationData=a;a=a.raiseNotificationViewedPath;"undefined"!==typeof a&&fetch(a,{mode:"no-cors"});b.waitUntil(self.registration.showNotification(c,
+    d))});
+function onClick(b,a,c){var d=!0;"action1"===b.action?("undefined"!==typeof c.notificationOptions.actions[0].deepLink&&(a+="&r="+encodeURIComponent(c.notificationOptions.actions[0].deepLink),d=!1),a+="&b="+encodeURIComponent("button1")):"action2"===b.action?("undefined"!==typeof c.notificationOptions.actions[1].deepLink&&(a+="&r="+encodeURIComponent(c.notificationOptions.actions[1].deepLink),d=!1),a+="&b="+encodeURIComponent("button2")):("undefined"!==typeof c.deepLink&&(a+="&r="+encodeURIComponent(c.deepLink),d=
+    !1),a+="&b="+encodeURIComponent("button0"));d?fireSilentRequest(a):clients.openWindow(a);b.notification.close()}self.addEventListener("notificationclick",function(b){var a=b.notification,c=a.data,d;"undefined"!==typeof c&&null!==c&&(d=c.wzrk_id);"undefined"===typeof d&&(d=a.title);a=localforage.getItem(d).then(function(a){a=JSON.parse(a);onClick(b,a.redirectPath,a)})["catch"](function(a){onClick(b,globalRedirectPath,globalNotificationData);console.log(a)});b.waitUntil(a)});
+var fireSilentRequest=function(b){fetch(b+"&s=true",{mode:"no-cors"})};
+
+//////////////////////////////////////////////////////////////////////////
 finalCart = {};
 
 var clevertap = {event:[],profile:[], account:[], onUserLogin:[],
@@ -6,6 +14,18 @@ var clevertap = {event:[],profile:[], account:[], onUserLogin:[],
 clevertap.account.push({"id": "WR5-K46-Z95Z"});
 clevertap.privacy.push({optOut: false});
 clevertap.privacy.push({useIP: true});
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAthOwP0h2io8QptvkixE71Aonluh9j5og",
+    authDomain: "avneesh-first-website.firebaseapp.com",
+    databaseURL: "https://avneesh-first-website.firebaseio.com",
+    projectId: "avneesh-first-website",
+    storageBucket: "avneesh-first-website.appspot.com",
+    messagingSenderId: "982610559404",
+    appId: "1:982610559404:web:cb07bbd9ec9c208c6e7a09"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 (function () {
 		 var wzrk = document.createElement('script');
@@ -72,6 +92,11 @@ function AddToCartFn(cartElmnt){
     'Product Quantity': c,
   });
 
-  alert(a+'/'+b+'/'+c+'/'+d);
+  clevertap.notifications.push({
+  "titleText":'Would you like to receive Push Notifications?',
+  "bodyText":'We promise to only send you relevant content and give you updates on your transactions',
+  "okButtonText":'Sign me up!',
+  "rejectButtonText":'No thanks',
+  "okButtonColor":'#f28046'});
 
 }
